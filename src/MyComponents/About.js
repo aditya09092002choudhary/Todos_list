5dc39e81498b8fcf13e2841a84cce320412e515f
aditya09092002choudhary <adityachoudhary1980@gmail.com>
2022-01-10 19:43:09 +0530

    update

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
+             
             <p>This is a react MyTodosList app. 
                 Make yourself focused by setting your goal.
             </p>
